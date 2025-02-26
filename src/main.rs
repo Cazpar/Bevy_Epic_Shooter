48b85039dd9605320b710018a849eefb3cdacc98
Cazpar <cstillinge@outlook.com>
2025-02-25 16:58:52 -0800

    Initial Setup, add game screen

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+// src/main.rs
+use bevy::prelude::*;
+
+mod plugins;
+mod components;
+mod resources;
+mod systems;
+mod events;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(plugins::GamePlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    // Setup camera and initial game state
+    commands.spawn(Camera2dBundle::default());
+}
